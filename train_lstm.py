c7e62a6109f8075e2e20923d26c469edce3c8981
Nico Ring <ringnico@web.de>
2017-01-25 14:15:14 +0100

    adds methods and main tests for train files

--- train_lstm.py
+++ train_lstm.py
@@ -1,12 +1,18 @@
 from goethe.corpora import LineNumbers
 from goethe.nnlm import TrainingData, RnnNNLM
+from gensim.models.word2vec import Word2Vec
 
-corpus = LineNumbers('preprocessed/large/numbers.txt')
-training_data = TrainingData(corpus)
-training_data.build_indices()
+def train_lstm_nnlm():
+    corpus = LineNumbers('preprocessed/large/numbers.txt')
+    training_data = TrainingData(corpus)
+    training_data.build_indices()
 
-word2vec = Word2Vec.load('models/preprocessed-tokens-300-cb.model')
-word2vec.init_sims(replace=True)
+    word2vec = Word2Vec.load('models/preprocessed-tokens-300-cb.model')
+    word2vec.init_sims(replace=True)
 
-rnn_nnlm = RnnNNLM(word2index=training_data.word2index, word2vec=word2vec, max_sequence_length=30)
-model = rnn_nnlm.train(training_data.sentence_dataset(), nb_epoch=10, samples_per_epoch=1000000)
+    rnn_nnlm = RnnNNLM(word2index=training_data.word2index, word2vec=word2vec, max_sequence_length=30)
+    model = rnn_nnlm.train(training_data.sentence_dataset(), nb_epoch=10, samples_per_epoch=1000000)
+    return model
+
+if __name__ == "__main__":
+    train_lstm_nnlm()
--- train_ngram.py
+++ train_ngram.py
@@ -1,13 +1,20 @@
 from goethe.corpora import LineNumbers
 from goethe.nnlm import TrainingData, NgramNNLM
+from gensim.models.word2vec import Word2Vec
 
-corpus = LineNumbers('preprocessed/large/numbers.txt')
-training_data = TrainingData(corpus)
-training_data.build_indices()
+def train_ngram_nnlm():
+    corpus = LineNumbers('preprocessed/large/numbers.txt')
+    training_data = TrainingData(corpus)
+    training_data.build_indices()
 
-word2vec = Word2Vec.load('models/preprocessed-tokens-300-cb.model')
-word2vec.init_sims(replace=True)
+    word2vec = Word2Vec.load('models/preprocessed-tokens-300-cb.model')
+    word2vec.init_sims(replace=True)
 
-nnlm = NgramNNLM(word2index=training_data.word2index, word2vec=word2vec)
+    nnlm = NgramNNLM(word2index=training_data.word2index, word2vec=word2vec)
 
-model = nnlm.train(training_data.ngram_dataset(3), model, nb_epoch=10, samples_per_epoch=1000000)
+    model = nnlm.train(training_data.ngram_dataset(3), model, nb_epoch=10, samples_per_epoch=1000000)
+    return model
+
+
+if __name__ == "__main__":
+    train_ngram_nnlm()
