2aa6cf7efd8862fa06333eda684602e8f18ed218
Nico Ring <ringnico@web.de>
2017-02-04 16:54:03 +0100

    updates gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -1,24 +1,23 @@
 import time
 import itertools as it
-import multiprocessing as mp
-from goethe.corpora import LeipzigCorpus
 from gensim.models import word2vec
+from goethe.corpora import Corpus
 
 model_config = {
-    'size': [100, 300, 600],
-    'iter': [5, 10, 20],
-    'sg': [0, 1]  # Skip-gram
+    'size': [200, 300, 400, 500, 600],
+    'window': [5, 10, 20],
+    'sg': [0, 1]  # Skip-gram or CBOW
 }
 
-sample_sizes = [int(n) for n in [1e6, 3e6, 10e6, 20e6]]
-
+sample_size = 10000000
+epochs = 10
 
 def train_model(config):
-    sample, size, epochs, sg = config
-    sentences = LeipzigCorpus('data', max_sentences=sample)
-    model = word2vec.Word2Vec(sentences=sentences, size=size, iter=epochs,
-                              workers=4)
-    name = 'n{}_size{}_epochs{}_sg{}'.format(sample, size, epochs, sg)
+    size, window, sg = config
+    sentences = LineSentence('../corpora/combined/combined.tokens.txt', limit=sample_size)
+    model = word2vec.Word2Vec(sentences=sentences, size=size, window=window,
+                              iter=epochs, workers=4)
+    name = 'n{}_size{}_epochs{}_sg{}_window{}'.format(sample_size, size, epochs, sg, window)
     return name, model
 
 
@@ -28,12 +27,10 @@ def minutes(t0):
 
 
 if __name__ == '__main__':
-    parameters = it.product(sample_sizes, model_config['size'],
-                            model_config['iter'], model_config['sg'])
+    parameters = it.product(model_config['size'], model_config['window']
+                            model_config['sg'])
     t0 = time.time()
     for p in parameters:
         name, model = train_model(p)
         model.save('models/' + name + '.model')
         print('{}\', saved model: {}'.format(minutes(t0), name))
-
-
