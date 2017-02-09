1f8e4c56708685610364f22635d182a177e00afb
Nico Ring <ringnico@web.de>
2017-02-09 17:32:42 +0100

    update lstm script

--- train_lstm.py
+++ train_lstm.py
@@ -1,34 +1,40 @@
-from goethe.nnlm import TrainingData, RnnNNLM
-from gensim.models.word2vec import Word2Vec
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 def train_lstm_nnlm(data_path, vector_file, epochs=1, batch_size=32,
                     restrict_vocab=10000, model_file=None):
-
+    from goethe.nnlm import TrainingData, RnnNNLM
+    from gensim.models.word2vec import Word2Vec
     # Fail early if h5py is not installed
     if model_file:
         import h5py
 
-    training_data = TrainingData.from_path(data_path, restrict_vocab=int(restrict_vocab))
+    training_data = TrainingData.from_path(data_path, restrict_vocab=restrict_vocab)
     word2vec = Word2Vec.load(vector_file)
     word2vec.init_sims(replace=True)
 
     rnn_nnlm = RnnNNLM(training_data, word2vec)
-    model = rnn_nnlm.train(int(epochs), int(batch_size))
+    model = rnn_nnlm.train(epochs, batch_size)
     rnn_nnlm.test(model)
     if model_file:
         model.save(model_file)
     return model
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-p", "--path", dest="data_path", metavar="PATH")
-    parser.add_option("-v", "--vectors", dest="vector_file", metavar="FILE")
-    parser.add_option("-s", "--save_file", dest="model_file", metavar="FILE")
-    parser.add_option("-e", "--epochs", dest="epochs", metavar="NUMBER")
-    parser.add_option("-b", "--batch-size", dest="batch_size", metavar="NUMBER")
-    parser.add_option("-r", "--restrict-vocab", dest="restrict_vocab", metavar="NUMBER")
+    parser = ArgumentParser(description='Train a Neural Language model with a LSTM.')
 
-    (options, args) = parser.parse_args()
-    param_dict = dict((k, v) for k, v in vars(options).items() if v)
+    parser.add_argument("-p", "--path", dest="data_path", metavar="DATA_PATH",
+        required=True, help="Path to directory containing training data.")
+    parser.add_argument("-v", "--vectors", dest="vector_file",
+        metavar="VECTORS_FILE", required=True, help="File with word vectors, built with gensim.")
+    parser.add_argument("-s", "--save_file", dest="model_file",
+        metavar="SAVE_MODEL_FILE", help="Filename to save the model.")
+    parser.add_argument("-e", "--epochs", dest="epochs", metavar="EPOCHS",
+        type=int, help="Number of epochs to run over all training data.")
+    parser.add_argument("-b", "--batch-size", dest="batch_size",
+        metavar="BATCH_SIZE", type=int, help="Number of sentences processed in one batch.")
+    parser.add_argument("-r", "--restrict-vocab", dest="restrict_vocab",
+        metavar="VOCABS_SIZE", type=int, help="Max vocab size. Can reduce memory consumption heaviliy")
+
+    args = parser.parse_args()
+    param_dict = dict((k, v) for k, v in vars(args).items() if v)
     train_lstm_nnlm(**param_dict)
