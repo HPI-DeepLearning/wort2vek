7697d71905550f11ba08036c919a2cf08114969e
Nico Ring <ringnico@web.de>
2017-03-13 10:55:25 +0100

    fix similarity script

--- goethe/eval/similarity.py
+++ goethe/eval/similarity.py
@@ -11,7 +11,7 @@ pairs_222 = 'pairs222.txt'
 pairs_350 = 'pairs350.txt'
 pairs_filenames = [pairs_65, pairs_222, pairs_350]
 
-def eval_similarity(model_filenames, save_path=None):
+def eval_similarity(model_filenames):
     for model_filename in model_filenames:
         model = KeyedVectors.load_word2vec_format(model_filename)
         rows = []
@@ -21,31 +21,31 @@ def eval_similarity(model_filenames, save_path=None):
             testset_name = pairs_filename.split(os.path.extsep)[0]
             row = [testset_name, sims[2], *sims[0], *sims[1]]
             rows.append(row)
+        model_name = os.path.basename(model_filename).split(os.path.extsep)[0]
         df = pd.DataFrame(rows, columns=header)
-        if save_path:
-            model_name = os.path.basename(model_filename).split(os.path.extsep)[0]
-            df.to_csv(os.path.join(save_path, model_name + '.similarity.csv'))
-        else:
-            yield df
+        yield model_name, df
 
 
 if __name__ == '__main__':
-   parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
        description='''Evaluate the Model using Pearson correlation coefficient
                       and Spearman rank-order correlation coefficient between
                       the similarities from the dataset and the similarities
                       produced by the model itself.''')
-   parser.add_argument('model_filenames', nargs='+',
+    parser.add_argument('model_filenames', nargs='+',
                        help='one or more models to be evaluated')
-   parser.add_argument('--save_path', help='path in which the results will be stored')
+    parser.add_argument('--output', help='path in which the results will be stored')
 
-   args = parser.parse_args()
-   models = args.model_filenames
-   save_path = args.save_path
+    args = parser.parse_args()
+    models = args.model_filenames
+    output = args.output
 
-   if save_path:
-       eval_similarity(models, save_path=save_path)
-   else:
-       dfs = list(eval_similarity(models))
-       for df in dfs:
-           print(df)
+    df_pairs = eval_similarity(models)
+
+    if output:
+        for name, df in df_pairs:
+            df.to_csv(os.path.join(output, name + '.similarity.csv'))
+    else:
+        for name, df in df_pairs:
+            print(name)
+            print(df)
