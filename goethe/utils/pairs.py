86cca699de55dfd15dd675fa0836f2fbd2c1ddcb
Nico Ring <ringnico@web.de>
2017-03-15 19:05:14 +0100

    pairs

--- goethe/utils/pairs.py
+++ goethe/utils/pairs.py
@@ -30,30 +30,41 @@ def word2vec(tokens, contextsize=5):
                                        for i, _ in enumerate(tokens)))
 
 
+def word_prob(word):
+    return 1
+
+
+def dep_prob(dep_name):
+    return 1
+
+
+def should_sample(word, level):
+    level_prob = 1 / level
+    word_prob = word_prob(word)
+    dep_prob = dep_prob(word.dep_)
+    return random.random() < level_prob
+
+
+def word_context(word):
+    candidates = deque()
+    seen = {word}
+
+    def add_to_queue(word, level):
+        for w in it.chain(word.children, [word.head]):
+            if w not in seen:
+                seen.add(w)
+                candidates.append((level, w))
+
+    add_to_queue(word, 1)
+    while candidates:
+        level, candidate = candidates.popleft()
+        if should_sample(candidate, level):
+            yield candidate
+        if level < MAX_LEVEL:
+            add_to_queue(candidate, level + 1)
+
+
 def squirrel(doc):
-
-    def should_sample(word, level):
-        level_prob = 1 / level
-        return random.random() < level_prob
-
-    def word_context(word):
-        candidates = deque()
-        seen = {word}
-
-        def add_to_queue(word, level):
-            for w in it.chain(word.children, [word.head]):
-                if not w in seen:
-                    seen.add(w)
-                    candidates.append((level, w))
-
-        add_to_queue(word, 1)
-        while candidates:
-            level, candidate = candidates.popleft()
-            if should_sample(candidate, level):
-                yield candidate
-            if level < MAX_LEVEL:
-                add_to_queue(candidate, level + 1)
-
     for word in doc:
         for ctx in word_context(word):
             yield (word.text, ctx.text)
