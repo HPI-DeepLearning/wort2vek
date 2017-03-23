018d28419dff228ac3eacce6da01779074690eb7
Nico Ring <ringnico@web.de>
2017-03-24 00:35:57 +0100

    adds window

---
+++ goethe/utils/pairs/window.py
@@ -0,0 +1,32 @@
+class Window:
+
+    PARSE_TREE = False
+
+    def __init__(self, **kwargs):
+        pass
+
+    def window_sort(self, tokens, n):
+        i = n - 1
+        j = n + 1
+        l = []
+        while i >= 0 or j < len(tokens):
+            if i >= 0:
+                l.append(tokens[i])
+            if j < len(tokens):
+                l.append(tokens[j])
+            i -= 1
+            j += 1
+        return l
+
+    def lines(self, doc):
+        tokens = [t.text for t in doc]
+        return [[tokens[i]] + self.window_sort(tokens, i)
+                for i in range(len(tokens))]
+
+
+if __name__ == '__main__':
+    t = ['Ich', 'bin', 'der', 'Nico', '.']
+    w = Window()
+    ls = w.token_list(t)
+    for l in ls:
+        print(l)
