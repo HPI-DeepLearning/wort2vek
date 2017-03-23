d852a39faea27c5e4fee793b5eabf0d22b693eba
Willi Raschkowski <willi@raschkowski.com>
2017-03-23 19:06:37 +0100

    Debug memory issue

--- goethe/utils/pairs/__main__.py
+++ goethe/utils/pairs/__main__.py
@@ -24,10 +24,12 @@ methods = {'squirrel': Squirrel,
 
 
 def contexts_for_lines(lines, method):
-    if method.PARSE_TREE:
-        nlp = spacy.load(LANG)
-    else:
-        nlp = spacy.load(LANG, parser=False)
+    # if method.PARSE_TREE:
+    #     nlp = spacy.load(LANG)
+    # else:
+    #     nlp = spacy.load(LANG, parser=False)
+    print(f'Received {len(lines)} lines')
+    nlp = spacy.load(LANG, parser=False)
     docs = nlp.pipe(lines, batch_size=BATCH_SIZE, n_threads=N_THREADS)
     return list(it.chain.from_iterable(method.lines(doc) for doc in docs))
 
@@ -50,11 +52,13 @@ if __name__ == '__main__':
             smart_open(args.output) as outf, \
             mp.Pool(PROCESSES) as pool:
         input_length = iterlen(inf)
+        print(f'Total length: {input_length}')
         inf.seek(0)
         lines = (l.strip() for l in inf)
-        for contexts in pool.map(ft.partial(contexts_for_lines, method=method),
-                                 chunks(lines, input_length, PROCESSES),
-                                 chunksize=1):
+        for contexts in pool.imap_unordered(ft.partial(contexts_for_lines, method=method),
+                                            chunks(lines, input_length, PROCESSES * 8),
+                                            chunksize=1):
+            print(f'Finished chunk of size {len(contexts)}')
             outputlines = (' '.join(context) + '\n'
                            for context in contexts)
             outf.writelines(outputlines)
