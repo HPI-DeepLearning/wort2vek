c614d35d822ce16b6de061ddc9ec27df2d9055b4
Willi Raschkowski <willi@raschkowski.com>
2017-03-31 14:45:37 +0200

    Add option to write line numbers

--- goethe/utils/context/__main__.py
+++ goethe/utils/context/__main__.py
@@ -7,6 +7,10 @@ import spacy
 from .methods import *
 from ..utils import args_to_kwargs, chunks, smart_open, iterlen
 
+description = """
+Generate context file. Takes a file with one sentence per line as input.
+Returns contexts either in `pairs` format or in `lines` format.
+"""
 
 LANG = 'de'
 BATCH_SIZE = 10000
@@ -21,36 +25,38 @@ methods = {'ttraverse': TreeTraverse,
            'levygoldberg': LevyGoldberg}
 
 
-def contexts_for_sents(sents, method, pairs=False):
-    contexts = method.pairs if pairs else method.lines
-    nlp = spacy.load(LANG)
-    docs = nlp.pipe(sents, batch_size=BATCH_SIZE, n_threads=N_THREADS)
-    for doc in docs:
-        yield from contexts(doc)
-
-
 def main(args):
     method = methods[args.method.lower()](**args_to_kwargs(args))
+    contextfunc = method.pairs if args.pairs else method.lines
+
+    nlp = spacy.load(LANG)
+
     with open(args.input) as inf, smart_open(args.output) as outf:
-        lines = (l.rstrip('\n') for l in inf)
-        contexts = contexts_for_sents(lines, method, pairs=args.pairs)
-        outputlines = (' '.join(c) + '\n' for c in contexts)
-        outf.writelines(outputlines)
+        sents = (l.rstrip('\n') for l in inf)
+        docs = nlp.pipe(sents, batch_size=BATCH_SIZE, n_threads=N_THREADS)
+        for i, doc in enumerate(docs):
+            contexts = contextfunc(doc)
+            contexts = (' '.join(c) + '\n' for c in contexts)
+            if args.numbers:
+                contexts = (f'{i}\t{c}' for c in contexts)
+            outf.writelines(contexts)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create context file')
+    parser = argparse.ArgumentParser(description=description)
     io = parser.add_argument_group('Input/Output')
     io.add_argument('-i', '--input', required=True)
     io.add_argument('-o', '--output')
+    parser.add_argument('-n', '--numbers', action='store_true',
+                        help='Add line numbers')
     method = parser.add_argument_group('Method')
     method.add_argument('-m', '--method', required=True,
                         choices=methods.keys())
     pairslists = method.add_mutually_exclusive_group(required=True)
     pairslists.add_argument('-p', '--pairs', action='store_true',
-                            help='Per line: One \'word context\' pair')
+                            help='Pairs format: per line one \'word context\' pair')
     pairslists.add_argument('-l', '--lists', action='store_true',
-                            help='Per line: \'word c1 c2 c3 ...\'')
+                            help='Lines format: per line \'word c1 c2 c3 ...\'')
     margs = parser.add_argument_group('Method arguments')
     margs.add_argument('-max_level', type=int, default=3)
     margs.add_argument('-window', type=int)
