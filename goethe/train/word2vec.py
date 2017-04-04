54a006905f8d92097d386d8d1223c0ce7d7e97dc
Willi Raschkowski <willi@raschkowski.com>
2017-04-04 23:08:54 +0000

    Fix limit=None error

--- goethe/train/word2vec.py
+++ goethe/train/word2vec.py
@@ -6,26 +6,27 @@ Script for training word vector models using public corpora
 """
 
 
-def word2vec(train, output, limit, **w2vargs):
-    limit = int(limit) if limit else None
-    sents = LineSentence(train, limit)
+def word2vec(train, output, limit=None, **w2vargs):
+    if limit:
+        sents = LineSentence(train, int(limit))
+    else:
+        sents = LineSentence(train)
     model = Word2Vec(sents, **w2vargs)
     model.wv.save_word2vec_format(output)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=description)
-
     parser.add_argument('-t', '--train', type=str, required=True,
                         help='token file')
     parser.add_argument('-o', '--output', type=str, required=True,
                         help='Target file to store model')
-    parser.add_argument('-limit', type=float, default=None,
+    parser.add_argument('-limit', type=float,
                         help='Number of sentences to use from data')
-    parser.add_argument('-workers', type=int, default=32,
+    parser.add_argument('-workers', type=int, default=8,
                         help='number of worker threads to train the model')
     modelargs = parser.add_argument_group(title='word2vec')
-    modelargs.add_argument('-iter', type=int, default=10,
+    modelargs.add_argument('-iter', type=int, default=5,
                            help='Number of iterations over the corpus')
     modelargs.add_argument('-size', type=int, default=400,
                            help='Size of word vectors')
