494876957a2804131b5ffab419ac2c1dd82e40c8
Willi Raschkowski <willi@raschkowski.com>
2017-04-06 21:22:11 +0000

    Adjust word2vecf parameters

--- goethe/train/word2vecf.py
+++ goethe/train/word2vecf.py
@@ -3,13 +3,8 @@ import argparse
 import subprocess
 import itertools as it
 
-# name =$2
-# cv = "$name.cv.txt"
-# wv = "$name.wv.txt"
-# . / count_and_filter - train $1 - cvocab $cv - wvocab $wv - min - count 100
-# . / word2vecf - train $1 - wvocab $wv - cvocab $cv - output "$name.vec" - dumpcv "$name.cv.vec" - size 300 - negative 15 - threads 10 - iters 10
 
-WORD2VEC_PATH = 'word2vecf'
+WORD2VEC_PATH = 'tools/word2vecf'
 COUNT_AND_FILTER = 'count_and_filter'
 WORD2VECF = 'word2vecf'
 
@@ -17,16 +12,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Word2vecf training')
     parser.add_argument('input', help='pairs file')
     parser.add_argument('output', help='name of outputted model')
-    parser.add_argument('-m', '--mincount', help='min count [100]', type=int, default=100)
-    parser.add_argument('-s', '--size', help='word vector size [300]', type=int, default=300)
+    parser.add_argument('-m', '--min_count', help='min count [100]', default=100)
+    parser.add_argument('-s', '--size', help='word vector size [300]', default=300)
     parser.add_argument('-n', '--negative',
-                        help='number of negative samples [15]', type=int, default=15)
-    parser.add_argument('-sub', '--subsample', type=str, default='1e-5',
+                        help='number of negative samples [15]', default=15)
+    parser.add_argument('-sub', '--subsample', default='1e-5',
                         help='Set threshold for frequency of words. Those that appear with higher frequency in the training data will be randomly down-sampled [1e-5]')
     parser.add_argument('-t', '--threads',
-                        help='number of threads [10]', type=int, default=10)
-    parser.add_argument(
-        '-i', '--iters', help='number of iterations [10]', type=int, default=10)
+                        help='number of threads [10]', default=10)
+    parser.add_argument('-i', '--iters',
+                        help='number of iterations [10]', default=10)
     args = parser.parse_args()
 
     word_vocab = f'{args.output}.cv.txt'
@@ -37,7 +32,7 @@ if __name__ == '__main__':
                          '-train':       args.input,
                          '-wvocab':      f'{args.output}.cv.txt',
                          '-cvocab':      f'{args.output}.wv.txt',
-                         '-min-count':   str(args.mincount),
+                         '-min-count':   str(args.min_count),
                      }.items())])
 
     subprocess.call([os.path.join(WORD2VEC_PATH, WORD2VECF),
